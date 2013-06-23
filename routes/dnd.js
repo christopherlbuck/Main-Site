8b26d09e8ba9f01a74e718ad9e9c11a9287a3ec7
Christopher Buck <christopherlbuck@gmail.com>
2013-06-22 22:40:39 -0400

    Updated to dnd/notes sql query
    Sql query updated to be the userName instead of just ID

--- routes/dnd.js
+++ routes/dnd.js
@@ -8,8 +8,13 @@ var common = require('../common.js'),
 	sqlEscape = common.sqlEscape;
 
 exports.dndNotes = function(req, res){
+	//Debug section
+	var d=true;
+	var pH='dndNotes-';
+
   //Get note list
-  var sql = 'select id,date_format(dateTime,\'%Y-%m-%d %r \') as \'Date added\' , note as Note, submittedByUserID as User from notes order by dateTime desc';
+  var sql = 'select notes.id,date_format(dateTime,\'%Y-%m-%d %r \') as \'Date added\' , note as Note, userName as User from dnd.notes as notes join serverinfo.userinfo as user on user.id=notes.submittedByUserID order by dateTime desc';
+  CL(d,pH+'sql:'+sql);
   sqlQuery('dnd',sql,true, function(fields,results){
 	var q1 = 'select id, name from notestag where active=1';
 	sqlQuery('dnd',q1, true, function(q1fields,q1results){
